0bd43873455ad3c9e960710c22a57d07798d68fa
Veronica Kim <vero.a.kim@gmail.com>
2018-02-10 23:45:32 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,24 +4,24 @@ def sentence?
     self.end_with?(".")
   end
 
-  def question?
-    self.end_with?("?")
-  end
-
-  def exclamation?
-    self.end_with?("!")
-  end
-
-#   def count_sentences
-#     if self.include?(sentence? || question? || exclamation?) == true
-#         self.split
-#     self.each do |element|
-#       element.length
-#         if element.length == nil
-#           return zero
-#         else
-#           element.length
-#       end
-#     end
-#     end
-# end
+#   def question?
+#     self.end_with?("?")
+#   end
+#
+#   def exclamation?
+#     self.end_with?("!")
+#   end
+#
+# #   def count_sentences
+# #     if self.include?(sentence? || question? || exclamation?) == true
+# #         self.split
+# #     self.each do |element|
+# #       element.length
+# #         if element.length == nil
+# #           return zero
+# #         else
+# #           element.length
+# #       end
+# #     end
+# #     end
+# # end
