f50b206e288612c3befff484569bb348d7be0703
Veronica Kim <vero.a.kim@gmail.com>
2018-02-10 23:47:08 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -6,14 +6,14 @@ def sentence?
     self.end_with?(".")
   end
 
-#   def question?
-#     self.end_with?("?")
-#   end
-#
-#   def exclamation?
-#     self.end_with?("!")
-#   end
-#
+  def question?
+    self.end_with?("?")
+  end
+
+  def exclamation?
+    self.end_with?("!")
+  end
+
 # #   def count_sentences
 # #     if self.include?(sentence? || question? || exclamation?) == true
 # #         self.split
