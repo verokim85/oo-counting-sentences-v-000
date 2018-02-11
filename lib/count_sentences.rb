d81b64df5fa1c4b67b3a90817f98d900ba8235de
Veronica Kim <vero.a.kim@gmail.com>
2018-02-11 00:21:27 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,8 +15,7 @@ def sentence?
   end
 
   def count_sentences
-  if self.end_with?("." || "?" || "!")
-       self.split("., ?, !")
+   self.split("., ?, !")
    self.each do |element|
      element.length
        if element.length == 0
