2d6ccbc44078de9e5d76aa0590c0afd6957eeade
Veronica Kim <vero.a.kim@gmail.com>
2018-02-11 00:35:32 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ def sentence?
   end
 
   def count_sentences
-   self.split if self.include?("." || "?" || "!")
+   self.split("." || "?" || "!")
    self.each do |element|
      element.length
        if element.length < 1
